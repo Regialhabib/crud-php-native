5ec84d1bc55e2805dfb7398cfd9f7b1fdbdb366a
Regi Al Habib <ffregi86@gmail.com>
2023-06-03 16:31:14 +0700

    first commit

---
+++ test.php
@@ -0,0 +1,15 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="" method="get">
+        <input type="color" name="color">
+        <input type="submit" name="submit">
+    </form>
+    <p><?php var_dump($_GET)?></p>
+</body>
+</html>
